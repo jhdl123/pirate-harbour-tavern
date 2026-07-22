75ae7ae7bcb924b42dd820239987c2a13e08c23f
jhdl123 <jhdl123@hotmail.com>
2026-07-22 22:22:20 +0100

    added item type

--- scenes/furniture/drinks_station.gd
+++ scenes/furniture/drinks_station.gd
@@ -1,9 +1,8 @@
 extends StaticBody2D
 
-
 func interact(player: Node) -> void:
-	if player.carrying_item == "":
-		player.set_carried_item("grog")
+	if player.carrying_item == ItemType.Type.NONE:
+		player.set_carried_item(ItemType.Type.GROG)
 		print("Picked up grog")
 	else:
 		print("Already carrying something")
--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -2,7 +2,7 @@ extends CharacterBody2D
 
 @export var movement_speed: float = 250.0
 
-var carrying_item: String = ""
+var carrying_item: ItemType.Type = ItemType.Type.NONE
 
 
 func _physics_process(_delta: float) -> void:
@@ -20,14 +20,16 @@ func _physics_process(_delta: float) -> void:
 func _process(_delta: float) -> void:
 	if Input.is_action_just_pressed("player_interact"):
 		try_interact()
-		
-func set_carried_item(item_name: String) -> void:
-	carrying_item = item_name
 
-	if carrying_item == "":
+
+func set_carried_item(item: ItemType.Type) -> void:
+	carrying_item = item
+
+	if carrying_item == ItemType.Type.NONE:
 		$CarriedItemSprite.visible = false
 	else:
 		$CarriedItemSprite.visible = true
+
 
 func try_interact() -> void:
 	var nearby_areas: Array[Area2D] = $InteractionDetector.get_overlapping_areas()
