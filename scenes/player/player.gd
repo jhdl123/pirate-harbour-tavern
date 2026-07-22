c09485ce639224246200ccf018a7ca5229d66773
jhdl123 <jhdl123@hotmail.com>
2026-07-22 16:20:12 +0100

    Add basic player movement and room boundaries

--- scenes/player/player.gd
+++ scenes/player/player.gd
@@ -5,10 +5,10 @@ extends CharacterBody2D
 
 func _physics_process(_delta: float) -> void:
 	var input_direction := Input.get_vector(
-		"move_left",
-		"move_right",
-		"move_up",
-		"move_down"
+		"player_move_left",
+		"player_move_right",
+		"player_move_up",
+		"player_move_down"
 	)
 
 	velocity = input_direction * movement_speed
